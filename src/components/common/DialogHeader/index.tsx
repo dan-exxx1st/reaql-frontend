694a3a02085528309028326acf281c096961b08d
Daniil Parfenov <dan.exxx1st@gmail.com>
2021-01-22 14:35:46 +0300

    Fix bugs.
    Fix the sourse of avatar and name in dialog header.

--- src/components/common/DialogHeader/index.tsx
+++ src/components/common/DialogHeader/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
 
 import { Typography, Wrapper } from 'components/UI';
 import { MessageSearch } from '..';
@@ -6,64 +6,82 @@ import { MessageSearch } from '..';
 import { StyledAvatar, StyledCircle, StyledDialogHeader } from './style';
 
 import { IDialogHeaderProps } from 'lib/types/components/common';
+import { useQuery } from '@apollo/client';
+import { Query } from 'lib/graphql/types';
+import { DIALOG } from 'lib/graphql/queries/dialog';
+import { UserContext } from 'helpers/contexts/userContext';
 
-const DialogHeader: FC<IDialogHeaderProps> = ({ className }) => {
+const DialogHeader: FC<IDialogHeaderProps> = ({ className, dialogId }) => {
     const [isShowMessageSearch, setIsShowMessageSearch] = useState(false);
+    const { state } = useContext(UserContext);
 
     const _handleToggleMessageSearch = () => {
         setIsShowMessageSearch(!isShowMessageSearch);
     };
 
-    // if (dialog) {
-    //     const { users, group } = dialog;
-    //     const name =
-    //         !group && users[0]
-    //             ? `${users[0].name} ${users[0].surname}`
-    //             : 'group!!!'; //!SERVER
-    //     const avatar = !group && users[0] ? users[0].avatar : ''; //!SERVER
+    const { data, loading } = useQuery<Query>(DIALOG, {
+        variables: {
+            dialogId,
+        },
+        fetchPolicy: 'no-cache',
+    });
 
-    return (
-        <StyledDialogHeader
-            className={className}
-            justifyContent="space-between"
-        >
-            {isShowMessageSearch ? (
-                <MessageSearch onClick={_handleToggleMessageSearch} />
-            ) : (
-                <>
-                    <Wrapper>
-                        <StyledAvatar src={''} />
-                        <Wrapper
-                            flexDirection="column"
-                            justifyContent="space-between"
-                            height="40px"
-                        >
-                            <Typography variant="body1" color="dc">
-                                {'name'}
-                            </Typography>
-                            <Typography variant="caption1" color="dgc">
-                                Today at 3:30 pm
-                            </Typography>
+    if (data && data.dialog && !loading && state && state.user) {
+        const { user } = state;
+        const {
+            dialog: { users, group },
+        } = data;
+        const otherUser = users.filter(
+            (dialogUser) => dialogUser?.id !== user.id
+        );
+        const name =
+            !group && otherUser[0]
+                ? `${otherUser[0].name} ${otherUser[0].surname}`
+                : 'group!!!'; //!SERVER
+        const avatar = !group && otherUser[0] ? otherUser[0].avatar : ''; //!SERVER
+
+        return (
+            <StyledDialogHeader
+                className={className}
+                justifyContent="space-between"
+            >
+                {isShowMessageSearch ? (
+                    <MessageSearch onClick={_handleToggleMessageSearch} />
+                ) : (
+                    <>
+                        <Wrapper>
+                            <StyledAvatar src={avatar} />
+                            <Wrapper
+                                flexDirection="column"
+                                justifyContent="space-between"
+                                height="40px"
+                            >
+                                <Typography variant="body1" color="dc">
+                                    {name}
+                                </Typography>
+                                <Typography variant="caption1" color="dgc">
+                                    Today at 3:30 pm
+                                </Typography>
+                            </Wrapper>
                         </Wrapper>
-                    </Wrapper>
-                    <Wrapper>
-                        <StyledCircle
-                            color="secondary"
-                            iconName="search"
-                            onClick={_handleToggleMessageSearch}
-                        />
-                        <StyledCircle
-                            color="secondary"
-                            iconName="threeDotsGrey"
-                        />
-                    </Wrapper>
-                </>
-            )}
-        </StyledDialogHeader>
-    );
-    // }
+                        <Wrapper>
+                            <StyledCircle
+                                color="secondary"
+                                iconName="search"
+                                onClick={_handleToggleMessageSearch}
+                            />
+                            <StyledCircle
+                                color="secondary"
+                                iconName="threeDotsGrey"
+                            />
+                        </Wrapper>
+                    </>
+                )}
+            </StyledDialogHeader>
+        );
+    }
 
-    // return <div>Loading...</div>;
+    return <div>Loading...</div>;
 };
 
 export default DialogHeader;
